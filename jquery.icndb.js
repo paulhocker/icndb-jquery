23837cabebc709e86b4fb1416167e923aa001dce
Paul Hocker <paul@spocker.net>
2019-06-23 11:12:53 -0500

    Update jquery.icndb.js

--- jquery.icndb.js
+++ jquery.icndb.js
@@ -7,7 +7,7 @@
 	$.icndb.client.id = 4;
 	$.icndb.client.version = 0.1;
 
-	var base = "//api.icndb.com/";
+	var base = "https://api.icndb.com/";
 
 	/**
 	 *	Returns the full URL of the given resource.
